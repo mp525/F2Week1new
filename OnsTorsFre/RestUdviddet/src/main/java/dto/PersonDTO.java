1b8fc6d5a62e175661405c39ef5aa3668f4258df
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-09-28 12:16:36 +0200

    first commit

---
+++ OnsTorsFre/RestUdviddet/src/main/java/dto/PersonDTO.java
@@ -0,0 +1,101 @@
+/*
+ *  To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import entities.Person;
+import java.util.Objects;
+
+/**
+ *
+ * @author Mathias
+ */
+public class PersonDTO {
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Integer id;
+
+    public PersonDTO() {
+    }
+
+    public PersonDTO(Person p) {
+        this.firstName = p.getFirstName();
+        this.lastName = p.getLastName();
+        this.phone = p.getPhone();
+        this.id = p.getId();
+    }
+
+    public PersonDTO(String firstName, String lastName, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+   
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PersonDTO other = (PersonDTO) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+}
